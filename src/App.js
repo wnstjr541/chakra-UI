284735e03fb6e98428dbf6facaf564ade1ebcb52
wnstjr541 <wnstjr541@naver.com>
2023-02-21 13:41:03 +0900

    chakra test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button, Tabs, TabList, TabPanels, Tab, TabPanel ,  Skeleton, SkeletonCircle, SkeletonText , Stack , Box} from '@chakra-ui/react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <section style={{width: "80vw" , margin : "0 auto" }}>
+      <p>버튼</p>
+      <div direction='row' spacing={4} align='center'>
+        <Button colorScheme='teal' variant='solid'>
+          Button
+        </Button>
+        {/* // variant로 스타일 변경 */}
+        <Button colorScheme='teal' variant='outline'>  
+          Button
+        </Button>
+        {/* // 사이즈 조절 */}
+        <Button colorScheme='teal' size='xs'> 
+          Button
+        </Button>
+        <Button
+          isLoading  // 로딩 중인 버튼
+          loadingText='Submitting'
+          colorScheme='teal'
+          variant='outline'
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Submit
+        </Button>
+      </div>
+
+      <div>
+        <p>탭</p>
+        <Tabs>
+          <TabList>
+            <Tab>One</Tab>
+            <Tab>Two</Tab>
+            <Tab>Three</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <p>one!</p>
+            </TabPanel>
+            <TabPanel>
+              <p>two!</p>
+            </TabPanel>
+            <TabPanel>
+              <p>three!</p>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </div>       
+      <br></br>
+      <div>
+        <p>스켈레톤 </p>
+        <Stack>
+          <Skeleton height='20px' />
+          <Skeleton height='20px' />
+          <Skeleton height='20px' />
+        </Stack>
+        
+        <p>스켈레톤 프로필</p>
+        <Box padding='6' boxShadow='lg' bg='white'>
+          <SkeletonCircle size='10' />
+          <SkeletonText mt='4' noOfLines={4} spacing='4' skeletonHeight='2' />
+        </Box>
+      </div>
+    </section>
+  
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,12 +3,27 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { extendTheme, ChakraProvider } from '@chakra-ui/react'
+
+// 2. `extendTheme` 적용하기
+const theme = extendTheme({
+  colors: {
+    brand: {
+      100: "#f7fafc",
+      // ...
+      900: "#1a202c",
+    },
+  },
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
+    <ChakraProvider >
+      <App />
+    </ChakraProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
